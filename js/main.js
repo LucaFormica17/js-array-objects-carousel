0f4fb8f530a50ecc8009f0e1a8620635022f9fe9
LucaFormica17 <145986784+LucaFormica17@users.noreply.github.com>
2023-12-11 13:08:45 +0100

    [UPDATE] milestone 1

--- js/main.js
+++ js/main.js
@@ -21,3 +21,47 @@ const images = [
         text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
     }
 ];
+
+//contiene immagini
+let active_image = '';
+
+//ciclo per mostrare immagini
+images.forEach((elem) => {
+    active_image += `<div class="item">
+    <img src="./${elem.image}" alt="image">
+    <div class="position-absolute bottom-50 text-right color-white padding-text">
+        <h2>${elem.title}</h2>
+        <h5>${elem.text}</h5>
+    </div>
+</div> `
+});
+
+//recupero contenitore immagini e appendo stringa
+const main_image = document.querySelector('.images').innerHTML = active_image;
+
+//salvo in variabile l'indice di partenza di array e assegno classe active
+let active_element = 0;
+let imagesDom = document.querySelectorAll('.item');
+//prendo solo il primo valore
+imagesDom[active_element].classList.add('active');
+
+//recupero pulsanti dal dom
+const nextBtn = document.querySelector('.next');
+const prevBtn = document.querySelector('.prev');
+
+
+//mettere in ascolto i pulsanti
+nextBtn.addEventListener('click', function(){
+    //prima rimuovo classe active
+    imagesDom[active_element].classList.remove('active');
+    //aumento element
+    active_element ++;
+    //assegno classe active
+    imagesDom[active_element].classList.add('active');
+});
+
+prevBtn.addEventListener('click', function(){
+    imagesDom[active_element].classList.remove('active');
+    active_element --;
+    imagesDom[active_element].classList.add('active');
+});
